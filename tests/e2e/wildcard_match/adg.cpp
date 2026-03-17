c0768441f1d7241dfbbdc99e2e5ab68f3d6d54a0
Sihao Liu <sihao@cs.ucla.edu>
2026-03-17 03:32:18 -0700

    Add wildcard_match e2e coverage

---
+++ tests/e2e/wildcard_match/adg.cpp
@@ -0,0 +1,36 @@
+//===-- adg.cpp - wildcard_match ADG generation ---------------*- C++ -*-===//
+
+#include "DomainADGs.h"
+
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/raw_ostream.h"
+
+static llvm::cl::opt<std::string> outputFile(
+    llvm::cl::Positional, llvm::cl::desc("<output .fabric.mlir file>"),
+    llvm::cl::init("wildcard_match.fabric.mlir"));
+
+int main(int argc, char **argv) {
+  llvm::cl::ParseCommandLineOptions(argc, argv,
+                                    "wildcard_match ADG generator\n");
+
+  auto parentPath = llvm::sys::path::parent_path(outputFile);
+  if (!parentPath.empty())
+    if (auto ec = llvm::sys::fs::create_directories(parentPath)) {
+      llvm::errs() << "error: cannot create output directory: " << parentPath
+                   << "\n";
+      return 1;
+    }
+
+  fcc::e2e::SpatialVectorDomainOptions opts;
+  opts.moduleName = "wildcard_match_domain";
+  opts.numPEs = 160;
+  opts.numExtMems = 3;
+  opts.numScalarInputs = 2;
+  opts.numScalarOutputs = 0;
+  opts.maxLdCount = 1;
+  opts.maxStCount = 1;
+  fcc::e2e::buildSpatialVectorDomain(outputFile, opts);
+  return 0;
+}
